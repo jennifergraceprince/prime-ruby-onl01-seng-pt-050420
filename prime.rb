96e5ddea0993c0c53d5129305e4ff753c97afb67
Jennifer Prince <jennifergraceprince@gmail.com>
2020-05-17 23:57:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
-def prime?(n)
-  for d in 2..(n - 1)
-   if (n % d) == 0
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
     return false
-#  if n == -1
-#    return false
-end
-end
-  true
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
 end
